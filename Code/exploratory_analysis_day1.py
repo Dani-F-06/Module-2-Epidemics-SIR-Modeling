97eb655544604b942912918f4862e54b3d81336d
dahhnie <vfd2ck@virginia.edu>
2026-02-24 13:02:24 -0500

    Update exploratory_analysis_day1.py
    Completed Homework from Thursdays Class

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -4,7 +4,19 @@ import matplotlib.pyplot as plt
 
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv(r'C:\Users\danie\OneDrive\Desktop\Comp Bme\Module 2\Module-2-Epidemics-SIR-Modeling\Data\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 
 #%%
 # Make a plot of the active cases over time
+plt.figure(figsize=(10, 6))
+plt.plot(data['date'], data['active reported daily cases'], marker='o', linestyle='-')
+plt.xlabel('Date')
+plt.ylabel('Active Cases')
+plt.title('Active Cases Over Time')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show() 
+
+# What do you notice about initial infections? The initial infections seem to be very low, with only a few cases reported in the early days. This could indicate that the virus was not spreading widely at the beginning, or that there was limited testing and reporting.
+# How could we measure how quickly its spreading? We could measure how quickly the virus is spreading by calculating the growth rate of active cases over time. This can be done by taking the difference in active cases between consecutive days and dividing it by the number of active cases on the previous day to get a percentage growth rate. 
+# What information about the virus would be helpful in determining the shape of the outbreak curve? Information about the virus's transmission rate, incubation period, and recovery time would be helpful in determining the shape of the outbreak curve.
