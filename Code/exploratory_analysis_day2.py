d8e6d054be6c50046de0565ae2ae1aa853198cd6
Sara Elster <selster@me.com>
2026-02-27 12:31:48 -0500

    finalizing data release 1 analysis

--- Code/exploratory_analysis_day2.py
+++ Code/exploratory_analysis_day2.py
@@ -15,17 +15,14 @@ def exponential_growth(t, r):
 # Fit the exponential growth model to the data. 
 # We'll use a handy function from scipy called CURVE_FIT that allows us to fit any given function to our data. 
 # We will fit the exponential growth function to the active cases data. HINT: Look up the documentation for curve_fit to see how to use it.
-x_data = data['day'].values
-y_data = data['active reported daily cases'].values
+x_data = data['day'].values.astype(float)
+y_data = data['active reported daily cases'].values.astype(float)
 popt, pcov = curve_fit(exponential_growth, x_data, y_data)
 
-print(x_data)
-print(y_data)
-
-r_fit = popt
+r_fit = popt[0]
 
 # Approximate R0 using this fit
-D = 3 # 3 day infectious period
+D = 6 # It seems from the data that there is a 6 day infectious period
 
 r0 = np.exp(r_fit * D)
 
@@ -33,17 +30,18 @@ print("Estimated growth rate R0: ", r0)
 
 # Add the fit as a line on top of your scatterplot.
 # Generate fitted curve
-y_fitted = exponential_growth(x_data, r0)
+y_fitted = exponential_growth(x_data, r_fit)
 
+# Plot fitted curve and actual data
 plt.figure(figsize=(10, 6))
-plt.plot(x_data, y_data, marker='o', linestyle='-')
+plt.scatter(x_data, y_data, label = "Actual Data")
 plt.plot(x_data, y_fitted, color = "red", label = "Estimated Curve")
 plt.xlabel('Day')
 plt.ylabel('Active Cases')
 plt.title('Exponential Growth Model Fit to Virus Data')
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.legend
+plt.legend()
 plt.show() 
 
 # %%
