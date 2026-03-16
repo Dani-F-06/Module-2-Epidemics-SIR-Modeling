61adc0f12b5dfa1c5177f1f4d7723843b999049a
Sara Elster <selster@me.com>
2026-03-15 21:07:00 -0400

    adding intervention code

--- Code/intervention_strategies.py
+++ Code/intervention_strategies.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from Euler_method import best_beta, best_sigma, best_gamma
 
 # Loading data from data release 3
 data_3 = pd.read_csv(r'/Users/saraelster/Desktop/UVA/Computational BME/Module 2/Module-2-Epidemics-SIR-Modeling/Data/mystery_virus_daily_active_counts_RELEASE#3.csv', parse_dates=['date'], header=0, index_col=None)
@@ -10,6 +9,10 @@ data_3 = pd.read_csv(r'/Users/saraelster/Desktop/UVA/Computational BME/Module 2/
 x_data_actual = data_3['day'].values.astype(float)
 y_data_actual = data_3['active reported daily cases'].values.astype(float)
 
+# Best values for beta, sigma, and gamma from Euler calculation
+best_beta = 0.25172413793103443
+best_sigma = 0.4854166666666666
+best_gamma = 0.07291666666666667
 
 # VT Infection Day 0-70
 def euler_seir(timepoints, N, S0, E0, I0, R0, beta, sigma, gamma):
@@ -39,12 +42,12 @@ def euler_seir(timepoints, N, S0, E0, I0, R0, beta, sigma, gamma):
     return S, E, I, R
 
 
-S0 = 38,857 # VT student population
+S0 = 38857 # VT student population
 E0 = 1
 I0 = 1
 R0 = 0
 timepoints = range(1,71)
-N = S0 + E0 + I0 + R0
+N = int(S0) + int(E0) + int(I0) + int(R0)
 
 S_fit, E_fit, I_fit, R_fit = euler_seir(
     timepoints, N, S0, E0, I0, R0,
@@ -60,8 +63,22 @@ plt.legend()
 plt.show()
 
 # Intervention strategy 1: Masking Mandates (starting at day 70) - reduces transmission by 40%
+new_beta = 0.4 * best_beta
+timepoints_2 = range(70,121)
 
+S_fit2, E_fit2, I_fit2, R_fit2 = euler_seir(
+    timepoints_2, N, S0, E0, I0, R0,
+    new_beta, best_sigma, best_gamma
+)
 
+plt.figure(figsize=(8, 5))
+plt.plot(timepoints, I_fit, label='No Masking Mandate')
+plt.plot(timepoints_2, I_fit2, label='Masking Mandate')
+plt.xlabel('Day')
+plt.ylabel('Population')
+plt.title('Modeling Effects of Masking Mandate on Infection Spread at VT')
+plt.legend()
+plt.show()
 
 # Intervention strategy 2: Testing/Quarantine (starting at day 70) - reduces infectious period by 2 days
 
