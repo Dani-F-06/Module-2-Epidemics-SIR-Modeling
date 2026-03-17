dbeb42aa755d60c3c94d679a8b5a7e1b3c1faaae
dahhnie <vfd2ck@virginia.edu>
2026-03-16 21:02:11 -0400

    Update intervention_strategies.py
    changing to general path before starting work

--- Code/intervention_strategies.py
+++ Code/intervention_strategies.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Loading data from data release 3
-data_3 = pd.read_csv(r'/Users/saraelster/Desktop/UVA/Computational BME/Module 2/Module-2-Epidemics-SIR-Modeling/Data/mystery_virus_daily_active_counts_RELEASE#3.csv', parse_dates=['date'], header=0, index_col=None)
+data_3 = pd.read_csv(r'Data\mystery_virus_daily_active_counts_RELEASE#3.csv', parse_dates=['date'], header=0, index_col=None)
 
 x_data_actual = data_3['day'].values.astype(float)
 y_data_actual = data_3['active reported daily cases'].values.astype(float)
@@ -85,3 +85,4 @@ plt.show()
 
 
 # Intervention strategy 3: 2 week school closure (starting at day 70) - day 70-84 - 20% normal contacts; day 84-120 - normal contacts
+
